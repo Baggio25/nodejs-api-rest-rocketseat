4834a61b061fefc9e119f4f56a94a3aa67352388
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-10 21:14:37 -0300

    Query Params

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ const PORT = 3333;
 
 const app = express();
 
-app.get("/products/:id/:user", (request, response) => {
+app.get("/products", (request, response) => {
 
-  const { id, user } = request.params;
-  response.send(`Produto: ${id}, Usuário: ${user}`);
+  // /products?page=1&limit=10
+
+  const { page, limit } = request.query;
+  response.send(`Página: ${page} de ${limit}`);
 
 });
 
