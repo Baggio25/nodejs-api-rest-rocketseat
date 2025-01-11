baae71b91c5fa5e9e8f2402e69b4b8aa78d7f96c
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-10 21:25:50 -0300

    Resposta com JSON e Status Code

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.get("/products", (request, response) => {
 
 app.post("/products", (request, response) => {
   const { name, price } = request.body;
-  response.send(`Produto: ${name}, custando R$ ${price}`);
+  response.status(201).json({ name, price });
 })
 
 app.listen(PORT, () => console.log(`**** Server is running on PORT: ${PORT} ****`));
