9b7927fd38e70fcc4b5a9b05ca97d6aeb86507e8
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-10 21:49:46 -0300

    Modificando requisição e arquivo d.ts

--- src/server.ts
+++ src/server.ts
@@ -9,20 +9,19 @@ app.use(express.json());
 // Middleware global
 //app.use(myMiddleware);
 
-
-//Middleware local em uma rota específica
-app.get("/products", myMiddleware, (request, response) => {
-
+app.get("/products", (request, response) => {
+  
   // /products?page=1&limit=10
-
+  
   const { page, limit } = request.query;
   response.send(`Página: ${page} de ${limit}`);
-
+  
 });
 
-app.post("/products", (request, response) => {
+//Middleware local em uma rota específica
+app.post("/products", myMiddleware, (request, response) => {
   const { name, price } = request.body;
-  response.status(201).json({ name, price });
+  response.status(201).json({ name, price, user_id: request.user_id });
 })
 
 app.listen(PORT, () => console.log(`**** Server is running on PORT: ${PORT} ****`));
