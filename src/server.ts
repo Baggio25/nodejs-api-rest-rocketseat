27ac4e33cce71e1c01422a369414084231630b30
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-10 21:00:09 -0300

    Enviando requisição com o método GET

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,9 @@ import express from "express";
 const PORT = 3333; 
 
 const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello World Express");
+});
+
 app.listen(PORT, () => console.log(`**** Server is running on PORT: ${PORT} ****`));
