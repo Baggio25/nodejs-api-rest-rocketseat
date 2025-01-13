823f132892d55c6a848f65fd6d9110f65c184719
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-13 14:23:13 -0300

    Tratando exceções

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction} from "express";
 import { routes } from "./routes";
 
 const PORT = 3333; 
 
 const app = express();
 app.use(express.json());
-
 app.use(routes);
+
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+  response.status(500).json({ message: error.message });
+});
 
 app.listen(PORT, () => console.log(`**** Server is running on PORT: ${PORT} ****`));
