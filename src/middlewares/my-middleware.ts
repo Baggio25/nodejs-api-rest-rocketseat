675e136e70fea7a1436d06e8a8f8a36711e7f510
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-01-13 15:14:48 -0300

    Formatando mensagens de erro

--- src/middlewares/my-middleware.ts
+++ src/middlewares/my-middleware.ts
@@ -2,8 +2,5 @@ import { Request, Response, NextFunction } from "express";
 
 export function myMiddleware(request: Request, response: Response, next: NextFunction) {
   request.user_id = "123456";
-  
-  console.log("Passou pelo MIDDLEWARE!");
-  
   return next();
 }
